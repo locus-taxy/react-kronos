c5f0ed1fee03d0118cb65e6f4869ef086f2eacc1
Anindya Ghosh <anindyag@locus.sh>
2019-08-14 00:45:52 +0530

    fix timezone issue for date

--- src/index.js
+++ src/index.js
@@ -315,13 +315,19 @@ class Kronos extends Component {
     let input = e.target.value
     let datetime = Moment(input, this.format(), true)
     if (datetime.isValid()) {
+
       // this changes is to create proper moment object with proper timezone.
       // So that, it does not mess up the date to isoString convertion.
       // In dropdown this.state.datetime is being used to calculate the time range,
       // so we are using this moment object to create a new moment object for the
-      // choosen time. {line no. : 323~324}
-      const [hour, minute] = datetime.format('HH:mm').split(':');
-      datetime = this.state.datetime.clone().set({hour, minute});
+      // choosen time/ date. {line no. : 324~333}
+      if (this.props.format === 'DD/MM') {
+        const [date, month] = datetime.format('DD/MM').split('/');
+        datetime = this.state.datetime.clone().set({date, month});
+      } else if (this.props.format === 'h:mm a') {
+        const [hour, minute] = datetime.format('HH:mm').split(':');
+        datetime = this.state.datetime.clone().set({hour, minute});
+      }
       this.save(datetime)
     } else if (input == '') {
       this.setState({
