b116a24783bc64945a9a32045e0dc3c77662a8ee
Mayank Agarwal <mayank@locus.sh>
2022-03-11 12:45:40 +0530

    cleanup configs

--- .babelrc.js
+++ .babelrc.js
@@ -1,7 +1,5 @@
 module.exports = {
   "plugins":[
-    // "@babel/plugin-transform-react-jsx",
-    // "@babel/plugin-proposal-class-properties",
     "@babel/plugin-proposal-function-bind",
     "transform-react-remove-prop-types"
   ],
