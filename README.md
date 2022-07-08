0dfdc8631231026778432b8f782d8ec18ea40566
geetgarg <geet@locus.sh>
2022-07-08 12:00:34 +0000

    Deploying to build/snyk-upgrade-e52ae3e85596e751d12a504c9d07a3c6 from @ locus-taxy/react-kronos@203cd1f030efe76dbd2b5f583d1190151e73f23a 🚀

---
+++ README.md
@@ -0,0 +1,87 @@
+React Kronos
+================
+
+A fast, intuitive, and elegant date and time picker for React.
+
+## Usage
+
+1. Install `yarn add @locus-taxy/react-kronos`
+3. Use
+```jsx
+import ReactKronos from '@locus-taxy/react-kronos';
+
+<ReactKronos
+  date={this.state.datetime}
+  onChange={this.onChange}
+/>
+```
+
+**Props:**
+- `date` - *Date()*, *Moment()*, *ISO*, or *string* (if string, must match `format`)
+- `time` - *Date()*, *Moment()*, *ISO*, or *string* (if string, must match `format`)
+- `timeStep` - *number* : minutes for time step (if not specified: 30 minutes)
+- `format` - *string* : [Moment formatting](http://momentjs.com/docs/#/parsing/string-format/) of date / time
+- `onChange` - *function* : native onChange method
+- `onChangeDateTime` - *function* : change method called when there is a new value
+- `returnAs` - *string* : onChange format `JS_DATE`, `MOMENT`, `ISO`, `STRING` (default: same as input)
+- `min` - *Date()*, *Moment()*, *ISO* to set as the minimum datetime
+- `max` - *Date()*, *Moment()*, *ISO* to set as the maximum datetime
+- `minTime` - *Date()*, *Moment()*, *ISO* to set as the minimum time (only the time will be used)
+- `maxTime` - *Date()*, *Moment()*, *ISO* to set as the maximum time (only the time will be used)
+- `closeOnSelect` - *boolean* : closes the dropdown when a value is selected (default: `true`)
+- `closeOnBlur` - *boolean* : closes the dropdown when the field is blurred (default: `true`)
+- `shouldTriggerOnChangeForDateTimeOutsideRange` - *boolean*: optionally allow dates outside min/max range to trigger onChanges (default: `false`)
+- `preventClickOnDateTimeOutsideRange` - *boolean*: optionally prevent users from clicking on dates outside min/max range (default: `false`)
+- `hideOutsideDateTimes` - *boolean*: optionally hide times that do not pass validation
+- `placeholder` - *string* : placeholder text when there is no value
+- `name` - *string* : name used for the input form
+- `disabled` - *boolean* : prevent interaction with input field
+- `inputStyle` - *object* : inline styles for input field
+- `inputClassName` - *string* : `.class` for input field
+- `inputId` - *string* : `#id` for input field
+- `calendarStyle` - *object* : inline styles for calendar
+- `calendarClassName` - *string* : `.class` for calendar
+- `options:`
+  - `color` - *string* : the highlight color in the UI as a hex
+  - `corners` - *number* : the pixel size of rounded corners (default: `4`)
+  - `font` - *string* : the font family (default: `Source Sans Pro`)
+  - `locale` - *object* : Moment locale [customization](http://momentjs.com/docs/#/customization/)
+    - `lang` - *string* : language (default: `en` for english)
+    - `settings` - *object* : properties to override as an object (default: `{ week: { dow: 1 }, weekdaysMin: ['M', 'T', 'W', 'T', 'F', 'S', 'S'] }`)
+  - `format`: - *object* : Moment formatting for cell titles
+    - `today`: - *string* : default: `Today`
+    - `year`: - *string* : default: `YYYY`
+    - `month`: - *string* : default: `MMM`
+    - `day`: - *string* : default: `D`
+    - `hour`: - *string* : default: `h:mm a`
+
+## Construction progress
+
+#### Roadmap
+
+**Needed**
+- Provide docs on website
+- options:
+  - date (restrict calendar to show only days, or with months, or with years)
+  - time (set increments, set ranges of time)
+- support for connected start & end date+time
+  - date (show range on calendar)
+  - time (show "0 hours", "0.5 hours", "1 hour", &c)
+
+#### Ideas
+
+- input field masking
+- ⇧+arrow uses the higher order unit
+- specify required props & non-required (what happens if none are included)
+- add warning messages to help use component properly
+- mobile support
+- wide browser compatibility
+- prop: `styles` - granular styling (overwrite any style, or provide entire theme)
+- add crazy awesome date/time interpretation/parsing similar to that in The Hit List
+- set up solid unit tests
+
+
+#### Thanks
+
+- **!!!** [gaearon](https://github.com/gaearon) big thanks to Dan Abramov who has built some incredible react components ([hot-loader](https://github.com/gaearon/react-hot-loader), [dnd](https://github.com/gaearon/react-dnd)), written excellent [articles](https://medium.com/@dan_abramov), and answered many questions
+- **!** [vjeux](https://github.com/vjeux) leading the CSS in JS movement
---
+++ es/index.js
@@ -0,0 +1,431 @@
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import Moment from 'moment';
+import cn from 'classnames';
+import { Keys, Levels, Units, Types } from './constants';
+import Calendar from './calendar';
+const ISOregex = /((\d{4}\-\d\d\-\d\d)[tT]([\d:\.]*)?)([zZ]|([+\-])(\d\d):?(\d\d))/;
+
+const minutesOfDay = m => {
+  return Moment(m).minutes() + Moment(m).hours() * 60;
+};
+
+class Kronos extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      datetime: this.getDateTimeInput().datetime,
+      input: this.getDateTimeInput().input,
+      type: this.getDateTimeInput().type,
+      visible: false,
+      level: this.getDefaultLevel()
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props != nextProps) {
+      this.validate(this.getDateTimeInput(nextProps).datetime, null, true);
+      this.setState({
+        datetime: this.getDateTimeInput(nextProps).datetime,
+        input: this.getDateTimeInput(nextProps).input
+      });
+    }
+  }
+
+  getDateTimeInput(props) {
+    props = props || this.props;
+    let prop = props.date || props.time || null;
+    let datetime, input, type;
+
+    if (prop === null) {
+      datetime = Moment();
+      input = null;
+      type = Types.MOMENT;
+    } else {
+      datetime = this.parse(prop);
+      input = datetime.format(this.format(props));
+
+      switch (typeof prop) {
+        case 'object':
+          if (Moment.isDate(prop)) {
+            type = Types.JS_DATE;
+          } else if (Moment.isMoment(prop)) {
+            type = Types.MOMENT;
+          } else {
+            type = null;
+          }
+
+          break;
+
+        case 'string':
+          if (prop.match(ISOregex)) {
+            type = Types.ISO;
+          } else {
+            type = Types.STRING;
+          }
+
+          break;
+      }
+    }
+
+    return {
+      datetime: datetime,
+      input: input,
+      type: type
+    };
+  }
+
+  getDefaultLevel() {
+    if (typeof this.props.date !== 'undefined') {
+      return Units.DAY;
+    } else if (typeof this.props.time !== 'undefined') {
+      return Units.HOUR;
+    } else {
+      console.warn('Please set a date or time prop. It can be null but not undefined.');
+      return Units.DAY;
+    }
+  }
+
+  format(props) {
+    props = props || this.props;
+
+    if (typeof props.format !== 'undefined') {
+      return props.format;
+    } else if (typeof props.date !== 'undefined') {
+      return 'MM-DD-YYYY';
+    } else if (typeof props.time !== 'undefined') {
+      return 'h:mm a';
+    } else {
+      return null;
+    }
+  }
+
+  toggle(visible) {
+    // Attempt to exit early
+    if (visible === this.state.visible) {
+      return;
+    }
+
+    if (typeof visible === 'undefined') {
+      visible = !this.state.visible;
+    }
+
+    if (visible !== this.state.visible) {
+      this.setState({
+        visible
+      });
+    }
+  }
+
+  parse(input) {
+    if (input === null) return null;
+    let parsing = Moment(input, this.format(), true);
+
+    if (!parsing.isValid()) {
+      let test = new Date(input);
+
+      if (isNaN(test.getTime())) {
+        test = this.state && this.state.datetime || Moment();
+      }
+
+      parsing = Moment(test);
+    }
+
+    return parsing;
+  }
+
+  save(saving) {
+    const {
+      datetime
+    } = this.state;
+
+    if (typeof this.props.date !== 'undefined') {
+      saving.hours(datetime.hours());
+      saving.minutes(datetime.minutes());
+    }
+
+    if (typeof this.props.time !== 'undefined') {
+      saving.date(datetime.date());
+      saving.month(datetime.month());
+      saving.year(datetime.year());
+    }
+
+    this.setState({
+      datetime: saving,
+      input: saving.format(this.format())
+    });
+    if (this.validate(saving, null, true)) this.commit(saving);
+  }
+
+  validate(datetime, timeUnit, isSaving) {
+    let outsideRange = false;
+
+    if (this.props.min && Moment(datetime).isBefore(this.props.min)) {
+      outsideRange = true;
+    }
+
+    if (this.props.max && Moment(datetime).isAfter(this.props.max)) {
+      outsideRange = true;
+    }
+
+    if (this.props.minTime && minutesOfDay(datetime) < minutesOfDay(this.props.minTime)) {
+      outsideRange = true;
+    }
+
+    if (this.props.maxTime && minutesOfDay(datetime) > minutesOfDay(this.props.maxTime)) {
+      outsideRange = true;
+    }
+
+    if (outsideRange && timeUnit !== 'hours') {
+      if (Moment(datetime).isSame(this.props.min, timeUnit) || Moment(datetime).isSame(this.props.max, timeUnit)) {
+        outsideRange = false;
+      }
+    }
+
+    if (isSaving) {
+      this.setState({
+        dateTimeExceedsValidRange: outsideRange
+      });
+      if (this.props.shouldTriggerOnChangeForDateTimeOutsideRange) return true;
+    }
+
+    return !outsideRange;
+  }
+
+  commit(datetime) {
+    let returnAs = this.props.returnAs || this.state.type;
+    let result;
+
+    switch (returnAs) {
+      case Types.ISO:
+        result = datetime.toISOString();
+        break;
+
+      case Types.JS_DATE:
+        result = datetime.toDate();
+        break;
+
+      case Types.MOMENT:
+        result = datetime;
+        break;
+
+      case Types.STRING:
+        result = datetime.format(this.format());
+        break;
+    }
+
+    this.props.onChangeDateTime && this.props.onChangeDateTime(result);
+  }
+
+  onClickInput(e) {
+    if (this.props.controlVisibility) {
+      if (this.props.onClick) this.props.onClick(e);
+    } else {
+      this.toggle(true);
+    }
+  }
+
+  onFocusInput(e) {
+    if (this.props.onFocus) this.props.onFocus(e);else {
+      this.toggle(true);
+    }
+  }
+
+  onBlurInput(e) {
+    let datetime = this.state.datetime || Moment();
+
+    if (this.above) {
+      ReactDOM.findDOMNode(this._input).focus();
+    } else if (this.props.closeOnBlur) {
+      this.toggle(false);
+      if (this.props.onBlur) this.props.onBlur(e);
+    }
+
+    if (this.state.input == this.state.datetime.format(this.format())) {
+      return;
+    } else {
+      datetime = this.parse(this.state.input);
+      if (datetime) this.save(datetime);
+    }
+  }
+
+  onChangeInput(e) {
+    if (this.props.onChange) this.props.onChange(e);
+    let input = e.target.value;
+    let datetime = Moment(input, this.format(), true);
+
+    if (datetime.isValid()) {
+      // this changes is to create proper moment object with proper timezone.
+      // So that, it does not mess up the date to isoString convertion.
+      // In dropdown this.state.datetime is being used to calculate the time range,
+      // so we are using this moment object to create a new moment object for the
+      // choosen time/ date. {line no. : 324~333}
+      if (this.props.format === 'DD/MM/YYYY') {
+        let [date, month, year] = datetime.format('DD/MM/YYYY').split('/');
+        [date, month, year] = [Number(date), Number(month) - 1, Number(year)];
+        datetime = this.state.datetime.clone().set({
+          date,
+          month,
+          year
+        });
+      } else if (this.props.format === 'DD/MM') {
+        let [date, month] = datetime.format('DD/MM').split('/');
+        [date, month] = [Number(date), Number(month) - 1];
+        datetime = this.state.datetime.clone().set({
+          date,
+          month
+        });
+      } else if (this.props.format === 'h:mm a') {
+        const [hour, minute] = datetime.format('HH:mm').split(':');
+        datetime = this.state.datetime.clone().set({
+          hour,
+          minute
+        });
+      }
+
+      this.save(datetime);
+    } else if (input == '') {
+      this.setState({
+        datetime: null,
+        input: ''
+      });
+      this.props.onChangeDateTime && this.props.onChangeDateTime(null);
+    } else {
+      this.setState({
+        input
+      });
+    }
+  }
+
+  onSelect(datetime, close, timeUnit) {
+    let shouldClose = close;
+    const {
+      visible
+    } = this.state;
+    const {
+      closeOnSelect,
+      preventClickOnDateTimeOutsideRange
+    } = this.props;
+
+    if (timeUnit) {
+      if (!this.validate(datetime, timeUnit.unit)) shouldClose = false;
+    } else {
+      if (!this.validate(datetime)) shouldClose = false;
+    }
+
+    if (close && shouldClose === false && preventClickOnDateTimeOutsideRange) {
+      return;
+    }
+
+    const willBeVisible = closeOnSelect && shouldClose ? !visible : visible;
+    this.setState({
+      visible: willBeVisible
+    });
+    this.save(datetime);
+
+    if (this.props.onSelect) {
+      this.props.onSelect(datetime, willBeVisible, shouldClose);
+    }
+  }
+
+  onKeyDown(code) {
+    let datetime = this.state.datetime || Moment();
+    let lvl = Levels[this.state.level];
+
+    switch (code) {
+      case Keys.UP:
+        this.onSelect(datetime.subtract(lvl.key.span, lvl.key.unit));
+        break;
+
+      case Keys.DOWN:
+        this.onSelect(datetime.add(lvl.key.span, lvl.key.unit));
+        break;
+
+      case Keys.ENTER:
+        if (lvl.down) {
+          this.setState({
+            level: lvl.down
+          });
+        } else {
+          if (this.state.input == datetime.format(this.format())) {
+            if (!this.validate(datetime)) {
+              this.toggle(true);
+            } else {
+              this.toggle();
+            }
+          } else {
+            if (!this.state.visible) this.toggle(true);
+            datetime = this.parse(this.state.input);
+            if (datetime) this.save(datetime);
+          }
+        }
+
+        break;
+    }
+  }
+
+  render() {
+    const mainClasses = cn('react-kronos', this.props.className, this.props.instance, this.props.theme.kronos, {
+      [this.props.theme.kronosDisabled]: this.props.disabled
+    });
+    const inputClasses = cn(this.props.inputClassName, this.props.theme.input, {
+      'outside-range': this.state.dateTimeExceedsValidRange
+    });
+    const visible = this.props.controlVisibility ? this.props.visible : this.state.visible;
+    return /*#__PURE__*/React.createElement("div", {
+      className: mainClasses,
+      "data-toolbox": "kronos"
+    }, /*#__PURE__*/React.createElement("input", {
+      type: "text",
+      id: this.props.inputId,
+      ref: input => this._input = input,
+      value: this.state.input || '',
+      onClick: this.onClickInput.bind(this),
+      onFocus: this.onFocusInput.bind(this),
+      onBlur: this.onBlurInput.bind(this),
+      onKeyDown: e => this.onKeyDown(e.keyCode),
+      onChange: this.onChangeInput.bind(this),
+      placeholder: this.props.placeholder,
+      name: this.props.name,
+      className: inputClasses,
+      disabled: this.props.disabled,
+      style: this.props.inputStyle
+    }), visible && /*#__PURE__*/React.createElement(Calendar, {
+      instance: this.props.instance,
+      datetime: this.state.datetime,
+      onSelect: this.onSelect.bind(this),
+      above: bool => typeof bool === 'undefined' ? this.above : this.above = bool,
+      level: this.state.level,
+      setLevel: level => this.setState({
+        level
+      }),
+      validate: this.validate.bind(this),
+      options: this.props.options,
+      inputRect: this._input.getClientRects()[0],
+      hideOutsideDateTimes: this.props.hideOutsideDateTimes,
+      timeStep: this.props.timeStep,
+      style: this.props.calendarStyle,
+      className: this.props.calendarClassName,
+      theme: this.props.theme
+    }));
+  }
+
+}
+
+_defineProperty(Kronos, "defaultProps", {
+  closeOnSelect: true,
+  closeOnBlur: true,
+  controlVisibility: false,
+  shouldTriggerOnChangeForDateTimeOutsideRange: false,
+  preventClickOnDateTimeOutsideRange: false,
+  visible: false,
+  disabled: false,
+  theme: {}
+});
+
+_defineProperty(Kronos, "above", false);
+
+export default Kronos;
